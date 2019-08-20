aa2aed0123ccdc08d994eeb5cdde715362c682a4
James Harvey <james.harvey@stonybrook.edu>
2019-08-20 19:39:53 -0400

    Column sum method

--- src/MultiDimensionArrays.java
+++ src/MultiDimensionArrays.java
@@ -12,12 +12,32 @@ public class MultiDimensionArrays {
 		}
 	}
 	
-	public static boolean isRagged(int [][] a) {
+	public static boolean isRagged(int [][] a) { //Ragged array is a multi-dimension array that isn't even
 		int l0 = a[0].length;
 		for(int[] row: a)
 			if(row.length != l0)
 				return true;
 		return false;
+	}
+	
+	public static int sum(int[][] matrix) {
+		int total = 0;
+		for(int[] row: matrix) {
+			for(int elem: row)
+				total += elem;
+		}
+		return total;
+	}
+	
+	public static void sumColumns(int[][] matrix) {
+		for(int c=0; c<matrix[0].length; c++) {
+			int total = 0;
+			
+			for(int r=0; r<matrix.length; r++) 
+				total += matrix[r][c];
+			
+			System.out.println("The total for column " + c + " is " + total);
+		}
 	}
 
 }
